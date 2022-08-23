44b59e5ed7b66898429579060106ff6387e08897
Leo Chen <lchen@equuscs.com>
2022-08-23 16:40:31 +0800

    init

---
+++ Firebase/Firebase.md
@@ -0,0 +1,25 @@
+# firebase login
+- npm install -g girebase-tools
+- firebase login
+    - this will open a browser window for you to log in
+- firebase init
+    - initialize your project
+- firebase deploy
+    - deploy the site to firebase
+	
+- firebase serve -p 8081
+
+# push notification
+web push notifications are a combination of web push and notifications. We can use push API to receive messages and then display them, using the notification API.
+- Push API
+	- The push API allows web apps to receive messages from a server, even when the website is not open.
+- Notification API
+	- The notification API enables websites to show push notifications to the user.
+- service worker
+	- Service workers allow code to be executed in the background without user interaction. 
+- Browser vendors
+	- Browser vendors: The middlemen for sending out push messages
+
+# Reference
+- https://medium.com/@david_shortman/build-and-debug-firebase-functions-in-vscode-73efb76166cf
+- https://felixgerschau.com/web-push-notifications-tutorial/
---
+++ Firebase/WebPush/public/firebase-messaging-sw.js
@@ -0,0 +1,76 @@
+// Import and configure the Firebase SDK
+
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.3/firebase-app.js";
+  import { getMessaging } from "https://www.gstatic.com/firebasejs/9.9.3/firebase-messaging.js";
+
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyCppRAKBnEwEZHKt2f2ssbl8GBx_navI4w",
+    authDomain: "webpushtest-e1b34.firebaseapp.com",
+    projectId: "webpushtest-e1b34",
+    storageBucket: "webpushtest-e1b34.appspot.com",
+    messagingSenderId: "182505393087",
+    appId: "1:182505393087:web:5475d3f747f8915a41ef0a",
+    measurementId: "G-5KNGG6F61J"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
+
+const messaging = firebase.messaging();
+
+/**
+ * Here is is the code snippet to initialize Firebase Messaging in the Service
+ * Worker when your app is not hosted on Firebase Hosting.
+
+ // Give the service worker access to Firebase Messaging.
+ // Note that you can only use Firebase Messaging here. Other Firebase libraries
+ // are not available in the service worker.
+ importScripts('https://www.gstatic.com/firebasejs/9.2.0/firebase-app-compat.js');
+ importScripts('https://www.gstatic.com/firebasejs/9.2.0/firebase-messaging-compat.js');
+
+ // Initialize the Firebase app in the service worker by passing in
+ // your app's Firebase config object.
+ // https://firebase.google.com/docs/web/setup#config-object
+ firebase.initializeApp({
+   apiKey: 'api-key',
+   authDomain: 'project-id.firebaseapp.com',
+   databaseURL: 'https://project-id.firebaseio.com',
+   projectId: 'project-id',
+   storageBucket: 'project-id.appspot.com',
+   messagingSenderId: 'sender-id',
+   appId: 'app-id',
+   measurementId: 'G-measurement-id',
+ });
+
+ // Retrieve an instance of Firebase Messaging so that it can handle background
+ // messages.
+ const messaging = firebase.messaging();
+ **/
+
+
+// If you would like to customize notifications that are received in the
+// background (Web app is closed or not in browser focus) then you should
+// implement this optional method.
+// Keep in mind that FCM will still show notification messages automatically 
+// and you should use data messages for custom notifications.
+// For more info see: 
+// https://firebase.google.com/docs/cloud-messaging/concept-options
+messaging.onBackgroundMessage(function(payload) {
+  console.log('[firebase-messaging-sw.js] Received background message ', payload);
+  // Customize notification here
+  const notificationTitle = 'Background Message Title';
+  const notificationOptions = {
+    body: 'Background Message body.',
+    icon: '/firebase-logo.png'
+  };
+
+  self.registration.showNotification(notificationTitle,
+    notificationOptions);
+});
